b5575e981daa33daf170bc7b436d08e3458f565c
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2024-07-22 21:53:45 +0530

    Create ClassExample.ts

---
+++ ClassExample.ts
@@ -0,0 +1,16 @@
+class Student{
+    name:string;
+    roll:number;
+    ispass:boolean;
+
+    constructor(n:string,r:number,p:boolean) {
+        this.name=n;
+        this.roll=r;
+        this.ispass=p
+    }
+    public display() : void{
+        console.log(this.name +"of roll num" + this.roll + "is" +this.ispass)
+    }
+}
+const s1= new Student("Avinash",11,true);
+s1.display();
